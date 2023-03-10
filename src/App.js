3403e9da72123e17032643f277c9721f3b2aa9c2
yingmu <wawamu2201@gmail.com>
2023-03-10 02:04:52 +0100

    layout and component finished

--- src/App.js
+++ src/App.js
@@ -1,23 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useState} from "react";
+import {AiOutlinePlus} from "react-icons/ai";
+import Todo from "./Todo";
+
+const style = {
+  bg: `h-screen w-screen p-4 bg-gradient-to-r from-[#2f80ed] to-[#1cb5e0]`
+
+}
 
 function App() {
+  const [todos, setTodos] = useState(["Prepare for side projects", "Finish the practicing projects"])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className={style.bg}>
+      <div className={style.container}>
+        <h3 className={style.heading}>TODO APP</h3>
+        <form className={style.form}>
+          <input className={style.input} type="text" placeholder="ADD TODO" />
+          <button className={style.button}><AiOutlinePlus size={30} /></button>
+        </form>
+        <ul>
+          {todos.map((todo, index) => (
+            <Todo key={index} todo={todo} />
+          ))}          
+        </ul>
+        <p className={style.count}>You have 2 todos</p>
+      </div>
     </div>
   );
 }
